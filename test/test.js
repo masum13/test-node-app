93d07304b77d862aba4b8b0e85e9c79d89834542
masum13 <patelmasum98@gmail.com>
2021-12-18 12:23:20 +0530

    update files

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var app = require('../index.js');
 describe('GET /', function() {
  it('respond with hello world', function(done) {
   //navigate to root and check the response is "hello world"
-  request(app).get('/').expect('hello world test jenkins app', done);
+  request(app).get('/').expect('hello world test node app', done);
  });
 });
 
